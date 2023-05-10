59826290253cd39a4c8f4ce0a05cc0d02577fb39
pilnystudent <tomasludvik@seznam.cz>
2023-05-10 11:49:40 +0200

    .

--- ERC20/IERC20.sol
+++ ERC20/IERC20.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v3.0.0/contracts/token/ERC20/IERC20.sol
 interface IERC20 {
     function totalSupply() external view returns (uint256);
 
