c33cbcac71d0c975659989068862fd6e0e58df4e
pilnystudent <tomasludvik@seznam.cz>
2023-05-10 16:34:12 +0200

    .

--- SimpleSwap.sol
+++ SimpleSwap.sol
@@ -15,7 +15,7 @@ contract SimpleSwap {
 
     Offer[] public offers;
 
-    function Add(
+    function add(
         IERC20 sell,
         uint256 sellAmount,
         IERC20 buy,
@@ -23,17 +23,22 @@ contract SimpleSwap {
     ) public returns (bool) {
         sell.transferFrom(msg.sender, address(this), sellAmount);
         offers.push(Offer(msg.sender, sell, sellAmount, buy, buyAmount, true));
+        emit Add(msg.sender, sell, sellAmount, buy, buyAmount);
         return true;
     }
 
-    function Exchange(uint256 index) public returns (bool) {
+    function exchange(uint256 index) public returns (bool) {
         Offer storage offer = offers[index];
         if (offer.active == true) {
             offer.sell.transfer(msg.sender, offer.sellAmount);
             offer.buy.transferFrom(msg.sender, offer.seller, offer.buyAmount);
             offer.active = false;
+            emit Exchange(msg.sender, offer.seller, offer.sell, offer.sellAmount, offer.buy, offer.buyAmount);
             return true;
         }
         return false;
     }
+
+    event Add(address indexed sender, IERC20 sell, uint256 sellAmount, IERC20 buy, uint256 buyAmount);
+    event Exchange(address indexed sender, address indexed recipient, IERC20 sell, uint256 sellAmount, IERC20 buy, uint256 buyAmount);
 }
