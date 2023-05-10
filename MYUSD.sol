819780e8490a24561938c77d6bb911587cad06b4
pilnystudent <tomasludvik@seznam.cz>
2023-05-10 11:47:38 +0200

    .

--- MYUSD.sol
+++ MYUSD.sol
@@ -5,11 +5,11 @@ import "./ERC20/ERC20.sol";
 
 contract MYUSD is ERC20 {
     function name() public pure returns (string memory) {
-        return "My USD stablecoin";
+        return "USD Example";
     }
 
     function symbol() public pure returns (string memory) {
-        return "MYUSD";
+        return "USDE";
     }
 
     function decimals() public pure returns (uint8) {
