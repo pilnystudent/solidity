d2efa30cf2e94e71c8df5cefa11e0755d29024fa
pilnystudent <tomasludvik@seznam.cz>
2023-05-10 16:40:17 +0200

    .

--- SimpleSwap.sol
+++ SimpleSwap.sol
@@ -5,40 +5,40 @@ import "./ERC20/ERC20.sol";
 
 contract SimpleSwap {
     struct Offer {
-        address seller;
-        IERC20 sell;
-        uint256 sellAmount;
-        IERC20 buy;
-        uint256 buyAmount;
+        address owner;
+        IERC20 selling;
+        uint256 sellingAmount;
+        IERC20 buying;
+        uint256 buyingAmount;
         bool active;
     }
 
     Offer[] public offers;
 
     function add(
-        IERC20 sell,
-        uint256 sellAmount,
-        IERC20 buy,
-        uint256 buyAmount
+        IERC20 selling,
+        uint256 sellingAmount,
+        IERC20 buying,
+        uint256 buyingAmount
     ) public returns (bool) {
-        sell.transferFrom(msg.sender, address(this), sellAmount);
-        offers.push(Offer(msg.sender, sell, sellAmount, buy, buyAmount, true));
-        emit Add(msg.sender, sell, sellAmount, buy, buyAmount);
+        selling.transferFrom(msg.sender, address(this), sellingAmount);
+        offers.push(Offer(msg.sender, selling, sellingAmount, buying, buyingAmount, true));
+        emit Add(msg.sender, selling, sellingAmount, buying, buyingAmount);
         return true;
     }
 
     function exchange(uint256 index) public returns (bool) {
         Offer storage offer = offers[index];
         if (offer.active == true) {
-            offer.sell.transfer(msg.sender, offer.sellAmount);
-            offer.buy.transferFrom(msg.sender, offer.seller, offer.buyAmount);
+            offer.selling.transfer(msg.sender, offer.sellingAmount);
+            offer.buying.transferFrom(msg.sender, offer.owner, offer.buyingAmount);
             offer.active = false;
-            emit Exchange(msg.sender, offer.seller, offer.sell, offer.sellAmount, offer.buy, offer.buyAmount);
+            emit Exchange(msg.sender, offer.owner, offer.selling, offer.sellingAmount, offer.buying, offer.buyingAmount);
             return true;
         }
         return false;
     }
 
-    event Add(address indexed sender, IERC20 sell, uint256 sellAmount, IERC20 buy, uint256 buyAmount);
-    event Exchange(address indexed sender, address indexed recipient, IERC20 sell, uint256 sellAmount, IERC20 buy, uint256 buyAmount);
+    event Add(address indexed owner, IERC20 selling, uint256 sellingAmount, IERC20 buying, uint256 buyingAmount);
+    event Exchange(address indexed sender, address indexed owner, IERC20 selling, uint256 sellingAmount, IERC20 buying, uint256 buyingAmount);
 }
