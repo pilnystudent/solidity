9f26bc78cbc4133f14bd47e09a7e4404904da34c
pilnystudent <tomasludvik@seznam.cz>
2023-05-10 13:21:50 +0200

    .

--- USDC.sol
+++ USDC.sol
@@ -4,12 +4,6 @@ pragma solidity ^0.8.17;
 import "./ERC20/ERC20.sol";
 
 contract USDC is ERC20 {
-    address public minter;
-
-    constructor() {
-        minter = msg.sender;
-    }
-
     function name() public pure returns (string memory) {
         return "USD Coin";
     }
@@ -22,13 +16,12 @@ contract USDC is ERC20 {
         return 18;
     }
 
-    function mint(address recipient, uint256 amount) external {
-        require(msg.sender == minter, "only minter can mint");
-        balanceOf[recipient] += amount;
+    function mint(uint256 amount) external {
+        balanceOf[msg.sender] += amount;
         unchecked {
             totalSupply += amount; // Cannot overflow
         }
-        emit Transfer(address(0), recipient, amount);
+        emit Transfer(address(0), msg.sender, amount);
     }
 
     function burn(uint256 amount) external {
