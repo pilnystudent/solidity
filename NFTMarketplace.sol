1724aa24c414e9c8c7849ced7aee0209b71ae65d
pilnystudent <tomasludvik@seznam.cz>
2023-05-15 18:07:34 +0200

    .

---
+++ NFTMarketplace.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+//https://github.com/rumble-kong-league/marketplace/blob/main/src/interfaces/IMarketplace.sol
+
+contract NFTMarketplace {}
