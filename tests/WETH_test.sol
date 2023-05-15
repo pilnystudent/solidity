d503a0cfed0f7987aed7d446311367954055873c
pilnystudent <tomasludvik@seznam.cz>
2023-05-15 15:16:20 +0200

    .

--- tests/WETH_test.sol
+++ tests/WETH_test.sol
@@ -1,18 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.4.22 <0.9.0;
 
-// This import is automatically injected by Remix
 import "remix_tests.sol";
-
-// This import is required to use custom transaction context
-// Although it may fail compilation in 'Solidity Compiler' plugin
-// But it will work fine in 'Solidity Unit Testing' plugin
-
 //import "remix_accounts.sol";
 import {WETH} from "../WETH.sol";
 
-// File name has to end with '_test.sol', this file can contain more than one testSuite contracts
 contract testSuite {
     WETH weth;
 
