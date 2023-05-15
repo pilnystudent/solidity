232d70c08cdcd88185f826813bc922d3bfd50a50
pilnystudent <tomasludvik@seznam.cz>
2023-05-15 17:22:09 +0200

    .

--- TNFT.sol
+++ TNFT.sol
@@ -7,7 +7,7 @@ import {Ownable} from "./access/Ownable.sol";
 
 contract TNFT is ERC721, Ownable {
     uint256 private counter;
-    mapping(uint256 => mapping(address => uint256)) private reward;
+    mapping(uint256 => mapping(address => uint256)) public reward;
 
     function mint() external onlyOwner {
         _mint(msg.sender, counter);
@@ -31,9 +31,5 @@ contract TNFT is ERC721, Ownable {
         require(msg.sender == _ownerOf[id]);
         require(reward[id][token] >= amount);
         IERC20(token).transfer(msg.sender, amount);
-    }
-
-    function rewardBalance(uint256 id, address token) public view returns (uint256) {
-        return reward[id][token];
     }
 }
