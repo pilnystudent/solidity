596711ec7c782203442f09f76224b9f765c2217e
pilnystudent <tomasludvik@seznam.cz>
2023-05-15 16:08:27 +0200

    .

--- tests/WETH_test.sol
+++ tests/WETH_test.sol
@@ -2,21 +2,29 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 import "remix_tests.sol";
-//import "remix_accounts.sol";
+import "remix_accounts.sol";
 import {WETH} from "../WETH.sol";
 
-contract testSuite {
-    WETH weth;
+contract testWETH is WETH {
+    address acc0;
 
-    function beforeEach() public {
-        weth = new WETH();
+    function beforeAll() public {
+        acc0 = TestsAccounts.getAccount(0);
     }
 
     function checkSymbol() public returns (bool) {
-        return Assert.equal(keccak256(abi.encodePacked(weth.symbol())), keccak256(abi.encodePacked("WETH")), "initial value is not correct");
+        return Assert.equal(keccak256(abi.encodePacked(this.symbol())), keccak256(abi.encodePacked("WETH")), "initial value is not correct");
     }
 
     function checkTotalSupply() public returns (bool) {
-        return Assert.equal(weth.totalSupply(), 0, "initial value is not correct");
+        return Assert.equal(this.totalSupply(), 0, "initial value is not correct");
+    }
+
+    /// #value: 500
+    function checkMintAndTransfer() public payable returns (bool) {
+        this.deposit{value: 500}();
+        Assert.equal(this.totalSupply(), 500, "total balance is not correct");
+        this.transfer(acc0, 100);
+        return Assert.equal(this.balanceOf(acc0), 100, "transfered value is not correct");
     }
 }
