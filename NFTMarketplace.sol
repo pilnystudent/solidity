bbf6570be3a193abebbdae85c6f6a101f9dd8ddb
pilnystudent <tomasludvik@seznam.cz>
2023-05-16 11:35:53 +0200

    .

--- NFTMarketplace.sol
+++ NFTMarketplace.sol
@@ -2,6 +2,38 @@
 pragma solidity ^0.8.17;
 
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-//https://github.com/rumble-kong-league/marketplace/blob/main/src/interfaces/IMarketplace.sol
 
-contract NFTMarketplace {}
+struct Ask {
+    address owner;
+    IERC721 nft;
+    uint256 nftId;
+    uint256 price;
+    bool active;
+}
+
+contract NFTMarketplace {
+    Ask[] public ask;
+
+    function createAsk(
+        IERC721 nft,
+        uint256 nftId,
+        uint256 price
+    ) external {
+        ask.push(Ask(msg.sender, nft, nftId, price, true));
+        nft.transferFrom(msg.sender, address(this), nftId);
+    }
+
+    function cancelAsk(uint256 askId) external {
+        require(ask[askId].owner == msg.sender);
+        require(ask[askId].active == true);
+        ask[askId].active = false;
+        ask[askId].nft.transferFrom(address(this), msg.sender, ask[askId].nftId);
+    }
+
+    function acceptAsk(uint256 askId) external payable {
+        require(ask[askId].price == msg.value, "price");
+        require(ask[askId].active == true, "active");
+        ask[askId].active = false;
+        ask[askId].nft.transferFrom(address(this), msg.sender, ask[askId].nftId);
+    }
+}
